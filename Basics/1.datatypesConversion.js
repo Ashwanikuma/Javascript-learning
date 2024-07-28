cb226f6edbc26d5a1650fa15cbbb3ba09fc8b234
ashwani kumar <120789408+Ashwanikuma@users.noreply.github.com>
2024-07-28 10:48:32 +0000

    data types conversions

---
+++ Basics/1.datatypesConversion.js
@@ -0,0 +1,21 @@
+let a=33;
+let b="33awai";
+
+let c= Number(b);
+
+let d=true;
+let e=false;
+console.log(typeof d);
+console.log(typeof e);
+
+// let g = Number(d);
+// let h = Number(e);
+
+let g = String(d);
+let h = String(e);
+console.log(g);
+console.log(h);
+
+
+console.log(typeof a, typeof b, typeof c);
+console.log(c);
