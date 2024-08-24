0fd080f365f72c9289334aeffdc72e773a2e6fdc
ashwani kumar <btechcsesscet@gmail.com>
2024-08-24 10:05:05 +0530

    arrow functions

---
+++ basics2/ArrowFunction.js
@@ -0,0 +1,11 @@
+const addNum = (num1, num2) => {
+  return num1 + num2;
+}
+
+console.log(addNum(3, 1));
+
+//implicit arrow functions
+const sub = (num1, num2) => (num1 - num2);
+
+console.log(addNum(4, 5));
+console.log(sub(5, 2));
